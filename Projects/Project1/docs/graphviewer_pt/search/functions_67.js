289e77be272450f3240a9b2254a040070d123772
Nay <dnpd.dd@gmail.com>
2013-04-20 03:04:08 +0100

    Add GraphViewer to the project, with docs and .jar copying to target dir

---
+++ Projects/Project1/docs/graphviewer_pt/search/functions_67.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['graphviewer',['GraphViewer',['../class_graph_viewer.html#aefca44052e0000ef416ddafc828f36cc',1,'GraphViewer::GraphViewer(int width, int height, bool port_n)'],['../class_graph_viewer.html#ad9d7b1d8b4ba8ef18517eae0e68568a2',1,'GraphViewer::GraphViewer(int width, int height, bool dynamic, int port_n)']]]
+];
