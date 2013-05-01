f18766e3a0dec67141ab2e1564023af04789cd7d
Nay <dnpd.dd@gmail.com>
2013-05-01 19:49:38 +0100

    Fix a bug with copy cctors, iterators and vector erases. Weird bugs are weird.

--- Projects/Project1/src/order.h
+++ Projects/Project1/src/order.h
@@ -8,15 +8,13 @@ class Order
 {
 public:
     Order(uint weight, uint volume) : _weight(weight), _volume(volume) { } ///< Constructor
-    Order(const Order& other) : _weight(other._weight), _volume(other._volume) { } ///< Copy constructor
-    Order operator=(const Order& other) const { return Order(other); } ///< Assignment operator
 
     uint GetWeight() const { return _weight; } ///< Returns the order weight
     uint GetVolume() const { return _volume; } ///< Returns the order volume
 
 private:
-    const uint _weight;
-    const uint _volume;
+    uint _weight;
+    uint _volume;
 };
 
 #endif // ORDER_H_
