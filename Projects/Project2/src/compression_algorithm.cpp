0178d34d94dd0d8a5b4ec1b13dec5673151352ce
Nay <dnpd.dd@gmail.com>
2013-05-17 02:39:50 +0100

    Proj 2: Add timing to CompressionAlgorithm::Compress and Decompress

--- Projects/Project2/src/compression_algorithm.cpp
+++ Projects/Project2/src/compression_algorithm.cpp
@@ -1,13 +1,23 @@
 #include "compression_algorithm.h"
 
+#include <ctime>
+
 
 bool CompressionAlgorithm::Compress(ByteBuffer& input, ByteBuffer& output, Benchmark& benchmark)
 {
     // init stats
 
+    clock_t t;
+
+    t = clock();
+
+    // compute
+
     bool result = CompressImpl(input, output);
 
     // end stats
+
+    double secs = t / (double) CLOCKS_PER_SEC;
 
     return result;
 }
@@ -16,9 +26,17 @@ bool CompressionAlgorithm::Decompress(ByteBuffer& input, ByteBuffer& output, Ben
 {
     // init stats
 
+    clock_t t;
+
+    t = clock();
+
+    // compute
+
     bool result = DecompressImpl(input, output);
 
     // end stats
+
+    double secs = t / (double) CLOCKS_PER_SEC;
 
     return result;
 }
