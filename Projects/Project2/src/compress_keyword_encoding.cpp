7bd703d294f9826357a6019aaa5dabe1d05f2ef9
Ruben Fernando Pinto Cordeiro <ruben.cordeiro.eng@gmail.com>
2013-05-22 22:57:48 +0100

    Refactor CompressKeywordEncoding::CompressImpl and CompressKeywordEncoding::DecompressImpl: now without fodas

--- Projects/Project2/src/compress_keyword_encoding.cpp
+++ Projects/Project2/src/compress_keyword_encoding.cpp
@@ -9,7 +9,7 @@
 std::string  initializeSymbols()
 {
 	std::string s;
-	for(int i = 32; i < 127; ++i)
+	for(int i = 0; i < 127; ++i)
 		s += i;
 
 	return s;
@@ -33,6 +33,7 @@ bool CompressKeywordEncoding::CompressImpl(const ByteBuffer& input, ByteBuffer& 
 
 	std::map<std::string, unsigned int>::iterator it;
 	std::map<std::string, unsigned int> stringFrequencies;
+
 	std::istringstream in((char *)input.Data());
 	std::string temp;
 	while(in.good())
@@ -46,34 +47,34 @@ bool CompressKeywordEncoding::CompressImpl(const ByteBuffer& input, ByteBuffer& 
 			stringFrequencies.insert(it, std::make_pair(temp, 1));
 	}
 
-	std::map<unsigned int, std::vector<std::string>> foda;
+	std::map<unsigned int, std::vector<std::string>> reverseStringFrequencies;
 	std::map<unsigned int, std::vector<std::string>>::iterator it1;
 
 	for (const auto& elem: stringFrequencies)
 	{
-		it1 = foda.find(elem.second);
-		if (it1 != foda.end())
+		it1 = reverseStringFrequencies.find(elem.second);
+		if (it1 != reverseStringFrequencies.end())
 		{
 			it1->second.push_back(elem.first);
 		}
 		else
 		{
-			it1 = foda.insert(it1, std::make_pair(elem.second, std::vector<std::string>()));
+			it1 = reverseStringFrequencies.insert(it1, std::make_pair(elem.second, std::vector<std::string>()));
 			it1->second.push_back(elem.first);
 		}
 	}
 
 	std::unordered_map<std::string, char> encodedStrings;
 	std::unordered_map<std::string, char>::iterator encodedStringsIt;
-	it1 = foda.begin();
-	for(std::string::iterator it_s = symbols.begin(); it_s != symbols.end() && it1 != foda.end(); ++it_s, ++it1)
+
+	it1 = reverseStringFrequencies.begin();
+	for(std::string::iterator it_s = symbols.begin(); it_s != symbols.end() && it1 != reverseStringFrequencies.end(); ++it_s, ++it1)
 		for(size_t i=0; i < it1->second.size(); ++i, ++it_s)
 		{
 			encodedStrings.insert(std::make_pair(it1->second[i], *it_s));
 			if(it_s == symbols.end())
 				break;
 		}
-
 
 		output.WriteDynInt(encodedStrings.size());
 		for(encodedStringsIt = encodedStrings.begin(); encodedStringsIt != encodedStrings.end(); ++encodedStringsIt)
@@ -88,24 +89,21 @@ bool CompressKeywordEncoding::CompressImpl(const ByteBuffer& input, ByteBuffer& 
 		{
 			in >> temp;
 			if((encodedStringsIt = encodedStrings.find(temp)) != encodedStrings.end())
-				output.WriteString(std::string(1,encodedStringsIt->second));//out << encodedStringsIt->second;
+				output.WriteString(std::string(1,encodedStringsIt->second));
 			else
-				output.WriteString(temp); //out << temp;
+				output.WriteString(temp); 
 		}
 
-		/*temp = out.str();
-		output.WriteBuffer(temp.c_str(), temp.size());*/
-		//output.WriteUInt8(0);
 		return true;
 }
 
 bool CompressKeywordEncoding::DecompressImpl(const ByteBuffer& input, ByteBuffer& output)
 {
 	ByteBuffer* hack = const_cast<ByteBuffer*>(&input);
-	
+
 	std::unordered_map<char, std::string> encodedSymbols;
 	std::unordered_map<char, std::string>::iterator encodedSymbolsIt;
-	
+
 	int numTableEntries;
 	numTableEntries = hack->ReadDynInt();
 
@@ -127,7 +125,6 @@ bool CompressKeywordEncoding::DecompressImpl(const ByteBuffer& input, ByteBuffer
 			output.WriteString(encodedSymbolsIt->second);
 		else
 			output.WriteString(temp);
-	} while(hack->GetReadPos() != hack->GetWritePos());
-
+	} while(hack->CanRead());
 	return true;
 }
