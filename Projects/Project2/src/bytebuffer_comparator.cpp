2cdca4bd3ffbb02054e052cd8cd6f8696f68f929
Nay <dnpd.dd@gmail.com>
2013-05-23 12:46:22 +0100

    Fix typo in ByteBufferComparator::Equal

--- Projects/Project2/src/bytebuffer_comparator.cpp
+++ Projects/Project2/src/bytebuffer_comparator.cpp
@@ -8,7 +8,7 @@ bool ByteBufferComparator::Equal(const ByteBuffer& b1, const ByteBuffer& b2)
     if (b1.Size() != b2.Size())
         return false;
 
-    return std::memcmp(b1.Data(), b2.Data(), b1.Size()) != 0;
+    return std::memcmp(b1.Data(), b2.Data(), b1.Size()) == 0;
 }
 
 double ByteBufferComparator::CompareSizes(const ByteBuffer& b1, const ByteBuffer& b2)
